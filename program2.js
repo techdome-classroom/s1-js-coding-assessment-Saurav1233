dad820390d697985a1d359d9ffa12372610f3134
SauravMishra <142394153+Saurav1233@users.noreply.github.com>
2024-09-27 12:57:57 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,6 +1,24 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+    let memo = {};
+
+    function dfs(i, j) {
+        if (memo.hasOwnProperty(`${i},${j}`)) return memo[`${i},${j}`];
+        if (i === s.length && j === p.length) return true;
+        if (j === p.length) return false;
+
+        let match = i < s.length && (s[i] === p[j] || p[j] === '?');
+
+        if (p[j] === '*') {
+            memo[`${i},${j}`] = dfs(i, j + 1) || (i < s.length && dfs(i + 1, j));
+        } else {
+            memo[`${i},${j}`] = match && dfs(i + 1, j + 1);
+        }
+
+        return memo[`${i},${j}`];
+    }
+
+    return dfs(0, 0);
 
   };
   
