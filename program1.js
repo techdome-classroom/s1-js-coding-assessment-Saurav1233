e1a5bde3bf5b7c51264a65985df48fb5c381c32f
SauravMishra <142394153+Saurav1233@users.noreply.github.com>
2024-09-27 12:56:20 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,21 +1,22 @@
 const getTotalIsles = function (grid) {
+
   if (!grid || grid.length === 0) return 0;
-  
+
   const rows = grid.length;
   const cols = grid[0].length;
   let islandCount = 0;
-  
-  const dfs = function(r, c) {
+
+  const dfs = function (r, c) {
     if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === 'W') {
       return;
     }
     grid[r][c] = 'W';
-    dfs(r - 1, c); // up
-    dfs(r + 1, c); // down
-    dfs(r, c - 1); // left
-    dfs(r, c + 1); // right
-  }
-  
+    dfs(r - 1, c);
+    dfs(r + 1, c);
+    dfs(r, c - 1);
+    dfs(r, c + 1);
+  };
+
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < cols; c++) {
       if (grid[r][c] === 'L') {
@@ -24,8 +25,8 @@ const getTotalIsles = function (grid) {
       }
     }
   }
-  
-  return islandCount;
-};
 
+  return islandCount;
+
+};
 module.exports = getTotalIsles;
